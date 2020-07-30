1adc38fd92be1a97a8292892fcfd327f78264bf3
ashkanfekridev <ashkanfekridev@gmail.com>
2020-07-30 10:36:10 +0430

    fic

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,8 +9,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./app/header"
+// import "./layout.css"
+import './../fonts/fonts.css';
+import './../styles/app.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
