431073974086f2dfcd879cb38fa538f0eca076f8
ashkanfekridev <ashkanfekridev@gmail.com>
2020-08-05 22:05:42 +0430

    fix display fullscreenand icon

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
+
+
+import Layout from "../components/layout";
+
+
 export default function Template({
                                    data, // this prop will be injected by the GraphQL query below.
                                  }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+  const {markdownRemark} = data // data.markdownRemark holds your post data
+  const {frontmatter, html} = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <Layout>
+      <div className="blog-post-container">
+        <article className="blog-post">
+          <header id="article-header">
+            <h2>{frontmatter.title}</h2>
+            <time>{frontmatter.date}</time>
+          </header>
+          <main id="article-main" className={'mt-4'}>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{__html: html}}
+            />
+          </main>
+
+        </article>
       </div>
-    </div>
+    </Layout>
   )
 }
 export const pageQuery = graphql`
